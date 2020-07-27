fc4d06a74d584fa396426eebd881c3b9e93cc421
charlazo94 <carlosanguloleandro@gmail.com>
2020-07-26 23:20:41 -0600

    finish css

--- src/codebase/weather.js
+++ src/codebase/weather.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Grid from '@material-ui/core/Grid';
 export default class Weather extends React.Component {
 
 
@@ -7,24 +7,36 @@ export default class Weather extends React.Component {
         return (<label>{name} <p>{data}</p></label>)
     }
 
+     justHour(time){
+        const date = new Date(time);
+        const localeSpecificTime = date.toLocaleTimeString();
+        return localeSpecificTime.replace(/:\d+ /, ' ');
+    }
+
+
     renderHourly(weather) {
 
-        return (<div>
-            {this.renderLabel('Hora', weather.dt_txt)}
+        return (<Grid item className='weather-detail'>
+            {this.renderLabel('Hora', this.justHour(weather.dt_txt))}
             {this.renderLabel('Se siente', weather.main.feels_like)}
             {this.renderLabel('Humedad', weather.main.humidity)}
             {this.renderLabel('Presion', weather.main.pressure)}
-            {this.renderLabel('Temperatura', weather.main.temp)}
-        </div>);
+            {this.renderLabel('Temp.', weather.main.temp)}
+        </Grid>);
     }
 
     render() {
 
 
         if (this.props.info.length > 0) {
-            return (<div>{this.props.info.map(wheater => {
-                return this.renderHourly(wheater)
-            })}</div>)
+            return (<div className='container'>
+                <Grid className='grid-day' container  spacing={2}>
+                    {this.props.info.map(wheater => {
+                        return this.renderHourly(wheater)
+
+                    })}
+                </Grid>
+            </div>)
         } else {
             return <div>Cargando: </div>
         }
